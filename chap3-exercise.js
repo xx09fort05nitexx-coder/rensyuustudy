916a2fe8d4c3c5ef4e8156c8e3a652a1b2850728
あっつー！ <xx09fort05nitexx@gmail.com>
2026-07-01 01:28:35 +0900

    jspromse練習

---
+++ chap3-exercise.js
@@ -0,0 +1,4 @@
+'use strict';
+
+const scores =[88,92,77,69,95];
+const adjustScores = scores.map((score) =>score*1.1)
--- rensyuu.js
+++ rensyuu.js
@@ -1,30 +1,8 @@
-alert( isNaN(NaN) ); // true
-alert( isNaN("str") ); // true
+const calculateSum = new Promise((resolve) => {
+    const result = 1 + 1; // 実際は時間がかかる処理
+    resolve(result);  // 計算できたら計算結果を返すよ〜
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(calculateSum);
 
 
